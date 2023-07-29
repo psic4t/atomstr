011c5e76a0f8543f089b258ff5d4822bd0886e11
psic4t <psic4t@data.haus>
2023-07-30 01:15:45 +0200

    add more relays

--- defines.go
+++ defines.go
@@ -9,11 +9,11 @@ import (
 
 var fetchInterval, _ = time.ParseDuration(getEnv("FETCH_INTERVAL", "15m"))
 var metadataInterval, _ = time.ParseDuration(getEnv("METADATA_INTERVAL", "2h"))
-var logLevel = getEnv("LOG_LEVEL", "INFO")
+var logLevel = getEnv("LOG_LEVEL", "DEBUG")
 var webserverPort = getEnv("WEBSERVER_PORT", "8061")
 var nip05Domain = getEnv("NIP05_DOMAIN", "atomstr.data.haus")
 var maxWorkers, _ = strconv.Atoi(getEnv("MAX_WORKERS", "5"))
-var r = getEnv("RELAYS_TO_PUBLISH_TO", "wss://nostr.data.haus")
+var r = getEnv("RELAYS_TO_PUBLISH_TO", "wss://nostr.data.haus, wss://spore.ws, wss://relayable.org")
 var relaysToPublishTo = strings.Split(r, ",")
 var defaultFeedImage = getEnv("DEFAULT_FEED_IMAGE", "https://void.cat/d/NDrSDe4QMx9jh6bD9LJwcK")
 var dbPath = getEnv("DB_PATH", "./atomstr.db")
